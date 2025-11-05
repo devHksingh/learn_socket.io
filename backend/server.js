abc64320573a5189809619637b948cb1350bfe30
devHksingh <hksingh.dev@gmail.com>
2025-11-05 18:33:34 +0530

    frontend setup

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,16 @@ import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
 
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user is connected",socket);
+  console.log("*********Socket ID***********");
+  console.log("SOCKET ID : ",socket.id);
+  
+  
+});
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
 });
