db1eda6d2c2759daafe001153d402fff7767f33b
devHksingh <hksingh.dev@gmail.com>
2025-11-25 17:41:22 +0530

    refactor: clean up websocket connection code and update socket initialization

--- frontend/src/ws.js
+++ frontend/src/ws.js
@@ -1,5 +1,14 @@
 import { io } from "socket.io-client";
 
-export function connectWS() {
-  return io('http://localhost:4002');
-}
+// export function connectWS() {
+//   return io('http://localhost:4002',{
+//     // transports: ['websocket'],
+//     // upgrade: false,
+//   })
+// }
+
+const socket = io("http://localhost:4000", {
+  autoConnect: false,
+});
+
+export default socket;
